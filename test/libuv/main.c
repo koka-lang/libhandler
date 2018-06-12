5d8adafa16ec139330c02725ddb2b0a92668c558
daanx <daan@microsoft.com>
2018-06-12 14:24:44 -0700

    wip: start on adding exceptions to async wrapper for libuv

--- test/libuv/main.c
+++ test/libuv/main.c
@@ -9,39 +9,58 @@
 
 typedef uv_handle_t* uv_handle_ptr;
 typedef uv_loop_t*   uv_loop_ptr;
+typedef uv_req_t*    uv_req_ptr;
 
 #define lh_uv_handle_ptr_value(v)   ((uv_handle_t*)lh_ptr_value(v))
 #define lh_value_uv_handle_ptr(h)   lh_value_ptr(h)
-#define lh_uv_loop_ptr_value(v)   ((uv_loop_t*)lh_ptr_value(v))
-#define lh_value_uv_loop_ptr(h)   lh_value_ptr(h)
+#define lh_uv_loop_ptr_value(v)     ((uv_loop_t*)lh_ptr_value(v))
+#define lh_value_uv_loop_ptr(h)     lh_value_ptr(h)
+#define lh_uv_req_ptr_value(v)      ((uv_req_t*)lh_ptr_value(v))
+#define lh_value_uv_req_ptr(r)      lh_value_ptr(r)
+
 
 LH_DEFINE_EFFECT2(async, uv_await, uv_loop);
-LH_DEFINE_OP1(async, uv_await, int, uv_handle_ptr);
 LH_DEFINE_OP0(async, uv_loop, uv_loop_ptr);
+LH_DEFINE_OP1(async, uv_await, int, uv_req_ptr);
 
+// Await a file system request
+int async_uv_await_fs(uv_fs_t* req) {
+  return async_uv_await((uv_req_t*)req);
+}
+
+// Check an error result, throwing on error
+void check_uv_err(int uverr) {
+  if (uverr < 0) {
+    lh_throw(lh_exception_alloc_strdup(uverr, uv_strerror(uverr)));
+  }
+}
 
 /*-----------------------------------------------------------------
   Async handler
 -----------------------------------------------------------------*/
 
+// The entry point for filesystem callbacks
 static void async_fs_cb(uv_fs_t* req) {
   lh_resume r = (lh_resume)(req->data);
   if (r != NULL) {
-    ssize_t err = req->result >= 0 ? 0 : req->result;
+    ssize_t err = (req->result >= 0 ? 0 : req->result);
     lh_release_resume(r, lh_value_uv_loop_ptr(req->loop), lh_value_int(err));
   }
 }
 
+// Await an asynchronous request
 static lh_value _async_uv_await(lh_resume r, lh_value local, lh_value arg) {
-  uv_handle_t* h = lh_uv_handle_ptr_value(arg);
-  h->data = r;
-  return lh_value_null;
+  uv_req_t* req = lh_uv_req_ptr_value(arg);
+  req->data = r;
+  return lh_value_null;  // this exits our async handler to the main event loop
 }
 
+// Return the current libUV event loop
 static lh_value _async_uv_loop(lh_resume r, lh_value local, lh_value arg) {
   return lh_tail_resume(r, local,local);
 }
 
+// The main async handler
 static const lh_operation _async_ops[] = {
   { LH_OP_GENERAL, LH_OPTAG(async,uv_await), &_async_uv_await},
   { LH_OP_TAIL_NOOP, LH_OPTAG(async,uv_loop), &_async_uv_loop},
@@ -68,89 +87,118 @@ static void uv_fs_free(uv_fs_t* req) {
   }
 }
   
-static int async_stat(const char* path, uv_stat_t* stat ) {
+static int async_statx(const char* path, uv_stat_t* stat ) {
   memset(stat, 0, sizeof(uv_stat_t));
   uv_fs_t*   req = uv_fs_alloc();
   uv_loop_t* loop = async_uv_loop();
   int err = uv_fs_stat(loop, req, path, &async_fs_cb);
   if (err == 0) {
-    err = async_uv_await(req);
+    err = async_uv_await_fs(req);
     if (err==0) *stat = req->statbuf;
   }
   uv_fs_free(req);
-  return err;
+  return err; // UV_UNKNOWN
 }
 
-static int async_fstat(uv_file file, uv_stat_t* stat) {
+uv_stat_t async_stat(const char* path) {
+  uv_stat_t stat;
+  check_uv_err(async_statx(path, &stat));
+  return stat;
+}
+
+static int async_fstatx(uv_file file, uv_stat_t* stat) {
   memset(stat, 0, sizeof(uv_stat_t));
   uv_fs_t*   req = uv_fs_alloc();
   uv_loop_t* loop = async_uv_loop();
   int err = uv_fs_fstat(loop, req, file, &async_fs_cb);
   if (err == 0) {
-    err = async_uv_await(req);
+    err = async_uv_await_fs(req);
     if (err == 0) *stat = req->statbuf;
   }
   uv_fs_free(req);
   return err;
 }
 
-static int async_fopen(const char* path, int flags, int mode, uv_file* file) {
+uv_stat_t async_fstat(uv_file file) {
+  uv_stat_t stat;
+  check_uv_err(async_fstatx(file, &stat));
+  return stat;
+}
+
+static int async_fopenx(const char* path, int flags, int mode, uv_file* file) {
   *file = -1;
   uv_fs_t* req = uv_fs_alloc();
   uv_loop_t* loop = async_uv_loop();
   int err = uv_fs_open(loop, req, path, flags, mode, &async_fs_cb);
   if (err == 0) {
-    err = async_uv_await(req);
-    if (err==0) *file = req->result; 
+    err = async_uv_await_fs(req);
+    if (err==0) *file = (uv_file)(req->result); 
   }
   uv_fs_free(req);
   return err;
 }
 
-static int async_fclose(uv_file file) {
+uv_file async_fopen(const char* path, int flags, int mode) {
+  uv_file file = -1;
+  check_uv_err(async_fopenx(path, flags, mode,&file));
+  return file;
+}
+
+
+static int async_fclosex(uv_file file) {
   if (file < 0) return 0;
   uv_fs_t*   req  = uv_fs_alloc();
   uv_loop_t* loop = async_uv_loop();
   int err = uv_fs_close(loop, req, file, &async_fs_cb);
-  if (err == 0) err = async_uv_await(req);
+  if (err == 0) err = async_uv_await_fs(req);
   uv_fs_free(req);
   return err;
 }
 
-static int async_fread(uv_file file, uv_buf_t* buf, int64_t offset, ssize_t* read ) {
+void async_fclose(uv_file file) {
+  check_uv_err(async_fclosex(file));
+}
+
+static int async_freadx(uv_file file, uv_buf_t* buf, int64_t offset, ssize_t* read ) {
   *read = 0;
   uv_fs_t* req = uv_fs_alloc();
   uv_loop_t* loop = async_uv_loop();
   int err = uv_fs_read(loop, req, file, buf, 1, offset, &async_fs_cb);
   if (err==0) {
-    err = async_uv_await(req);
+    err = async_uv_await_fs(req);
     if (err==0) *read = req->result;
   }
   uv_fs_free(req);
   return err;
 }
 
-static int async_fread_full(const char* path, ssize_t* len, char** contents ) {
+ssize_t async_fread(uv_file file, uv_buf_t* buf, int64_t offset) {
+  ssize_t read = 0;
+  check_uv_err(async_freadx(file, buf, offset, &read));
+  return read;
+}
+
+static int async_fread_fullx(const char* path, ssize_t* len, char** contents ) {
   *contents = NULL;
   *len = 0;
   uv_file file;
-  int err = async_fopen(path, O_RDONLY, 0, &file);
+  int err = async_fopenx(path, O_RDONLY, 0, &file);
   if (err != 0) return err;
   uv_stat_t stat;
-  err = async_fstat(file, &stat);
+  err = async_fstatx(file, &stat);
   if (err == 0) {
     ssize_t size   = stat.st_size;
     char*   buffer = (char*)malloc(size + 1);
-    uv_buf_t buf   = uv_buf_init(buffer, size);
+    uv_buf_t buf   = uv_buf_init(buffer, (unsigned)size);
     ssize_t read = 0;
     ssize_t total = 0;
-    while (total < size && (err = async_fread(file, &buf, -1, &read)) == 0 && read > 0) {
+    while (total < size && (err = async_freadx(file, &buf, -1, &read)) == 0 && read > 0) {
       total += read;
       if (total > size) {
         total = size;
       }
       else {
-        buf = uv_buf_init(buffer + total, size - total);
+        buf = uv_buf_init(buffer + total, (unsigned)(size - total));
       }
     }
     buffer[total] = 0;
@@ -162,8 +210,15 @@ static int async_fread_full(const char* path, ssize_t* len, char** contents ) {
       free(buffer);
     }
   }
-  async_fclose(file);
+  async_fclosex(file);
   return err;
+}
+
+char* async_fread_full(const char* path) {
+  char*   contents = NULL;
+  ssize_t len = 0;
+  check_uv_err(async_fread_fullx(path, &len, &contents));
+  return contents;
 }
 
 /*-----------------------------------------------------------------
@@ -171,28 +226,15 @@ static int async_fread_full(const char* path, ssize_t* len, char** contents ) {
 -----------------------------------------------------------------*/
 static void test_stat() {
   const char* path = "cenv.h";
-  uv_stat_t stat;
-  int err = async_stat(path,&stat);
-  if (err==0) {
-    printf("file %s last access time: %li\n", path, stat.st_atim.tv_sec);
-  }
-  else {
-    fprintf(stderr, "stat error: %i: %s\n", err, uv_strerror(err));
-  }
+  uv_stat_t stat = async_stat(path);
+  printf("file %s last access time: %li\n", path, stat.st_atim.tv_sec);  
 }
 
 static void test_fileread() {
   printf("opening file\n");
-  char* contents;
-  ssize_t len;
-  int err = async_fread_full("cenv.h", &len, &contents);
-  if (err == 0) {
-    printf("read %li bytes:\n%s\n", len, contents);
-    free(contents);
-  }
-  else {
-    fprintf(stderr, "error: %i: %s\n", err, uv_strerror(err));
-  }
+  char* contents = async_fread_full("cenv.h");
+  printf("read %Ii bytes:\n%s\n", strlen(contents), contents);
+  free(contents);  
 }
 
 static void uv_main() {
@@ -211,9 +253,19 @@ static lh_value uv_main_action(lh_value arg) {
   return lh_value_null;
 }
 
+static lh_value uv_main_try_action(lh_value arg) {
+  lh_exception* exn;
+  lh_try(&exn, uv_main_action, arg);
+  if (exn != NULL) {
+    printf("unhandled exception: %s\n", exn->msg);
+    lh_exception_free(exn);
+  }
+  return lh_value_null;
+}
+
 static void uv_main_cb(uv_timer_t* t_start) {
   // uv_mainx(t_start->loop);
-  async_handler(t_start->loop, &uv_main_action, lh_value_null);
+  async_handler(t_start->loop, &uv_main_try_action, lh_value_null);
   uv_timer_stop(t_start);
 }
 
