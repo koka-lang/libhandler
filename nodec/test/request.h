c9642befda665a1c15b294377f48d311790958b7
REDMOND\kirko <kirko@microsoft.com>
2018-07-17 08:40:05 -0700

    Changed code to standard format

--- nodec/test/request.h
+++ nodec/test/request.h
@@ -4,77 +4,77 @@
 #include "sbuf.h"
 #include "kvp.h"
 
-/*****************************************************************************\
-*   string_t                                                                  *
-*                                                                             *
-*       a safe string containing a pointer to the start of the string         *
-*       along with the length of the string not including any terminator      *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    string_t
+
+        a safe string containing a pointer to the start of the string
+        along with the length of the string not including any terminator
+-----------------------------------------------------------------------------*/
 typedef struct _string_t {
-	const char* s;
-	size_t len;
+  const char* s;
+  size_t len;
 } string_t;
 
-/*****************************************************************************\
-*   header_t                                                                  *
-*                                                                             *
-*       for iterating through HTTP request headers                            *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    header_t
+
+        for iterating through HTTP request headers
+-----------------------------------------------------------------------------*/
 typedef struct _header_t {
-    string_t field;
-    string_t value;
+  string_t field;
+  string_t value;
 } header_t;
 
-/*****************************************************************************\
-*   http_parser_callback                                                      *
-*                                                                             *
-*       list of all possible http_parser_callbacks                            *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    http_parser_callback
+
+        list of all possible http_parser_callbacks
+-----------------------------------------------------------------------------*/
 typedef enum _http_parser_callback {
-    ON_NONE = 0,
-    ON_MESSAGE_BEGIN,
-    ON_URL,
-    ON_STATUS,
-    ON_HEADER_FIELD,
-    ON_HEADER_VALUE,
-    ON_HEADERS_COMPLETE,
-    ON_BODY,
-    ON_MESSAGE_COMPLETE,
-    ON_CHUNK_HEADER,
-    ON_CHUNK_COMPLETE
+  ON_NONE = 0,
+  ON_MESSAGE_BEGIN,
+  ON_URL,
+  ON_STATUS,
+  ON_HEADER_FIELD,
+  ON_HEADER_VALUE,
+  ON_HEADERS_COMPLETE,
+  ON_BODY,
+  ON_MESSAGE_COMPLETE,
+  ON_CHUNK_HEADER,
+  ON_CHUNK_COMPLETE
 } http_parser_callback;
 
-/*****************************************************************************\
-*   http_request_t                                                            *
-*                                                                             *
-*       contains all the data for processing the HTTP request                 *
-*       holds stat for the http_parser callback functions                     *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    http_request_t
+
+        contains all the data for processing the HTTP request
+        holds stat for the http_parser callback functions
+-----------------------------------------------------------------------------*/
 typedef struct _http_request_t {
-    http_parser parser;
-	http_parser_settings settings;
-	bool headers_are_complete;
-	uint64_t content_length;    // -1 if not set in headers
-    sbuf_t sbuf;
-    size_t sbuf_inc;    // charcter chunk increment
-	kvp_t kvp;
-    kvpbuf_t kvpbuf;
-    size_t kvpbuf_inc;  // increment of the number of key-value pairs
-    pascal_string_t url;
-    http_parser_callback previous;
-    bool connection_close;
-    bool body_is_final;
+  http_parser parser;
+  http_parser_settings settings;
+  bool headers_are_complete;
+  uint64_t content_length;    // -1 if not set in headers
+  sbuf_t sbuf;
+  size_t sbuf_inc;    // charcter chunk increment
+  kvp_t kvp;
+  kvpbuf_t kvpbuf;
+  size_t kvpbuf_inc;  // increment of the number of key-value pairs
+  pascal_string_t url;
+  http_parser_callback previous;
+  bool connection_close;
+  bool body_is_final;
 } http_request_t;
 
-/*****************************************************************************\
-*   default array sizes                                                       *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    default array sizes
+-----------------------------------------------------------------------------*/
 #define HTTP_REQUEST_DEFAULT_SBUF_INC 32
 #define HTTP_REQUEST_DEFAULT_KVPBUF_INC 2
 
-/*****************************************************************************\
-*   exports                                                                   *
-\*****************************************************************************/
+/*-----------------------------------------------------------------------------
+    exports
+-----------------------------------------------------------------------------*/
 http_request_t* http_request_alloc();
 void http_request_free(http_request_t* self);
 size_t http_request_execute( http_request_t* self, const char* data, size_t length);
