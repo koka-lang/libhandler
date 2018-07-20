b179d19234fd9777439b5ac64d0869eedf102a93
REDMOND\kirko <kirko@microsoft.com>
2018-07-20 14:42:27 -0700

    Added testing http requests in files

--- nodec/test/main-client.c
+++ nodec/test/main-client.c
@@ -73,13 +73,67 @@ void test_as_client_one_byte_at_a_time() {
   }}
 }
 
+static long file_get_size(FILE* file) {
+  const long original_position = ftell(file);
+  fseek(file, 0, SEEK_END);
+  const long ans = ftell(file);
+  fseek(file, original_position, SEEK_SET);
+  return ans;
+}
+
+/*-----------------------------------------------------------------
+  test_read_file
+
+  reads the contents of a file into an uv_buf_t
+-----------------------------------------------------------------*/
+
+static uv_buf_t read_file(const char* path) {
+  uv_buf_t ans = { 0, 0 };
+  FILE* file = 0;
+  const errno_t rc = fopen_s(&file, path, "r");
+  if (rc == 0 && file != 0) {
+    const long len = file_get_size(file);
+    ans = nodec_buf_alloc(len);
+    fread(ans.base, ans.len, 1, file);
+    fclose(file);
+  }
+  else if (rc != 0 && file == 0) {
+    nodec_throw(123); // what to put here?
+  }
+  return ans;
+}
+
+/*-----------------------------------------------------------------
+  test_client_file
+
+  Reads an HTTP client request from a file and sends it to
+  the server.
+-----------------------------------------------------------------*/
+
+static void test_client_file(const char* path) {
+  uv_buf_t buf = read_file(path);
+  {with_free(buf.base) {
+    uv_stream_t* conn = async_tcp_connect("127.0.0.1", "8080");
+    {with_stream(conn) {
+      async_write_buf(conn, buf);
+      printf("await response...\n");
+      char* body = async_read_all(conn);
+      {with_free(body) {
+        printf("received:\n%s", body);
+      }}
+    }}
+  }}
+}
+
 /*-----------------------------------------------------------------
   Main
 -----------------------------------------------------------------*/
 
 static void entry() {
   printf("in the main loop\n");
-  test_as_client_one_byte_at_a_time();
+  //test_as_client_one_byte_at_a_time();
+  const char* path = ".\\..\\..\\nodec\\test\\example1.txt";
+  test_client_file(path);
 }
 
 int main() {
