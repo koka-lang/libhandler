2bd8f8e5130c677159f098f9b428c49760dd1634
REDMOND\kirko <kirko@microsoft.com>
2018-07-20 10:08:05 -0700

    Added test that sends one byte at a time

--- nodec/test/main-client.c
+++ nodec/test/main-client.c
@@ -32,12 +32,38 @@ void test_as_client() {
 }
 
 /*-----------------------------------------------------------------
+test_as_client2
+
+Attempt to break the system by sending a request one byte at a
+time.
+-----------------------------------------------------------------*/
+
+void test_as_client2() {
+  uv_stream_t* conn = async_tcp_connect("127.0.0.1", "8080");
+  {with_stream(conn) {
+    const char* s;
+    for (size_t i = 0; (s = http_request_parts[i]) != NULL; i++) {
+      for (const char* pch = s; *pch; pch++) {
+        uv_buf_t buf = { 1, (char*)pch };
+        async_write_buf(conn, buf); // send one byte
+        async_wait(100); // put in a delay to force a new chunk
+      }
+    }
+    printf("await response...\n");
+    char* body = async_read_all(conn);
+    {with_free(body) {
+      printf("received:\n%s", body);
+    }}
+  }}
+}
+
+/*-----------------------------------------------------------------
   Main
 -----------------------------------------------------------------*/
 
 static void entry() {
   printf("in the main loop\n");
-  test_as_client();
+  test_as_client2();
 }
 
 int main() {
