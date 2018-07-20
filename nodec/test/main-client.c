0a3908810d9bb435f0df5861a1966c535152cd7d
REDMOND\kirko <kirko@microsoft.com>
2018-07-20 11:33:22 -0700

    refactoring one byte at a time test

--- nodec/test/main-client.c
+++ nodec/test/main-client.c
@@ -32,28 +32,44 @@ void test_as_client() {
 }
 
 /*-----------------------------------------------------------------
-test_as_client2
+  write_one_char
 
-Attempt to break the system by sending a request one byte at a
-time.
+  Writes a single character to the stream
 -----------------------------------------------------------------*/
 
-void test_as_client2() {
+static void write_one_char(uv_stream_t* conn, const char* pch) {
+  uv_buf_t buf = { 1, (char*)pch };
+  async_write_buf(conn, buf); // send one byte
+  async_wait(100); // put in a delay to force a new chunk
+}
+
+/*-----------------------------------------------------------------
+  test_as_client2_connection
+
+  Attempt to break the system by sending a request one byte at a
+  time. Called by test_as_client2
+-----------------------------------------------------------------*/
+
+static void test_as_client2_connection(uv_stream_t* conn) {
+  const char* s;
+  for (size_t i = 0; (s = http_request_parts[i]) != NULL; i++)
+    for (const char* pch = s; *pch; pch++)
+      write_one_char(conn, pch);
+  printf("await response...\n");
+  char* body = async_read_all(conn);
+  {with_free(body) {
+    printf("received:\n%s", body);
+  }}
+}
+
+/*-----------------------------------------------------------------
+  test_as_client_one_byte_at_a_time
+-----------------------------------------------------------------*/
+
+void test_as_client_one_byte_at_a_time() {
   uv_stream_t* conn = async_tcp_connect("127.0.0.1", "8080");
   {with_stream(conn) {
-    const char* s;
-    for (size_t i = 0; (s = http_request_parts[i]) != NULL; i++) {
-      for (const char* pch = s; *pch; pch++) {
-        uv_buf_t buf = { 1, (char*)pch };
-        async_write_buf(conn, buf); // send one byte
-        async_wait(100); // put in a delay to force a new chunk
-      }
-    }
-    printf("await response...\n");
-    char* body = async_read_all(conn);
-    {with_free(body) {
-      printf("received:\n%s", body);
-    }}
+    test_as_client2_connection(conn);
   }}
 }
 
@@ -63,7 +79,7 @@ void test_as_client2() {
 
 static void entry() {
   printf("in the main loop\n");
-  test_as_client2();
+  test_as_client_one_byte_at_a_time();
 }
 
 int main() {
